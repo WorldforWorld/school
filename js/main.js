85ebc424fa9ba6e5f32339c98fd2dc353d9ba413
Yuri Kuzmichev <2091god@mail.ru>
2020-04-09 21:57:26 +0500

    fix: fix button

--- js/main.js
+++ js/main.js
@@ -4,7 +4,57 @@ var swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+// Слайдер сервисы платформ
+var appendNumber = 4;
+var prependNumber = 1;
+var swiper = new Swiper('.services__container', {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+var swiper = new Swiper('.services__container__catalog', {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
+// Способ доставки
+var curr = $('.product__deliverymethod__first');
+$('.product__deliverymethod__nav input[type="radio"]').on('change', function() {
+  curr.hide();
+  curr = $('.' + $('.product__deliverymethod__nav input[type="radio"]:checked').val() );
+  curr.show();
+});
+// Пагинация
+var items = $('.list-wrapper .list-item');
+    var numItems = items.length;
+    var perPage = 15;
+
+    items.slice(perPage).hide();
+
+    $('#pagination-container').pagination({
+      items: 300,
+      itemsOnPage: 10,
+      // items: numItems,
+      // itemsOnPage: perPage,
+      prevText: "&laquo;",
+      nextText: "&raquo;",
+      onPageClick: function (pageNumber) {
+        var showFrom = perPage * (pageNumber - 1);
+        var showTo = showFrom + perPage;
+        items.hide().slice(showFrom, showTo).show();
+      }
+    });
+
+// Часы --------------------------------------------------
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
@@ -47,16 +97,4 @@ function initializeClock(id, endtime) {
 var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
 initializeClock('countdown', deadline);
 
-
-// Слайдер сервисы платформ
-var appendNumber = 4;
-var prependNumber = 1;
-var swiper = new Swiper('.services__container', {
-  slidesPerView: 3,
-  centeredSlides: true,
-  spaceBetween: 30,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+console.log('я работаю');
